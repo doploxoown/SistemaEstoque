0a9de95025deccc39da6cc12e23f43e88c3c9dcb
Thiago Figueira <thi.figueira@hotmail.com>
2018-09-25 13:58:21 -0300

    Login no Sistema com Banco de Dados

--- ControleEstoque.Web/Controllers/ContaController.cs
+++ ControleEstoque.Web/Controllers/ContaController.cs
@@ -26,7 +26,7 @@ namespace ControleEstoque.Web.Controllers
                 return View(login);
             }
 
-            var achou = (login.Usuario == "thiago" && login.Senha == "123");
+            var achou = (UsuarioModel.ValidarUsuario(login.Usuario, login.Senha));
 
             if (achou)
             {
